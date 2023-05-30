e610e7c7837e9a3902ac4e99311909a3df917807
pshyampra <nothing@example.com>
2023-05-30 16:04:07 +0000

    second modification to script

--- automation.sh
+++ automation.sh
@@ -26,3 +26,16 @@ cp shyamp-httpd-logs-${timestamp}.tar /tmp/.
 
 aws s3 cp /tmp/shyamp-httpd-logs-${timestamp}.tar s3://upgrad-shyamp/shyamp-httpd-logs-${timestamp}.tar
 
+filepath="/tmp/shyamp-httpd-logs-${timestamp}.tar"
+size=$( stat -c %s $filepath)
+if test -f "/var/www/html/inventory.html"; then
+   echo "inventory file exists"
+else echo "Logtype        datecreated        type       size" >> /var/www/html/inventory.html
+fi
+
+echo "httpd-logs	$timestamp	  tar	    $size" >> /var/www/html/inventory.html
+
+if test -f "/etc/cron.d/automation"; then
+   echo "cronfile exists"
+else echo "0 1 * * * root /root/Automation_project/Automation_project/automation.sh" > /etc/cron.d/automation
+fi
